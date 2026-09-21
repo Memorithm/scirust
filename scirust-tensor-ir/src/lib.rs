5b90bc12944a0ba0cafad7c53d7723593ba48856
MEMOPERF <contact@checkupauto.fr>
2026-09-21 03:24:58 -0700

    chore(stack): rebase exact cost exports on IR-E2/E7

--- scirust-tensor-ir/src/lib.rs
+++ scirust-tensor-ir/src/lib.rs
@@ -32,8 +32,8 @@ pub use autodiff::jvp as linearize;
 pub use autodiff::{AutodiffError, GradGraph, JvpGraph, VjpGraph, grad, jvp, value_and_grad, vjp};
 pub use canonical_identity::{
     CanonicalIdentityError, GRAPH_STRUCTURAL_IDENTITY_V1, REPRESENTATION_ANCHOR_IDENTITY_V1,
-    REPRESENTATION_PLAN_IDENTITY_V1, canonical_graph_bytes,
-    canonical_representation_anchor_bytes, canonical_representation_plan_bytes,
+    REPRESENTATION_PLAN_IDENTITY_V1, canonical_graph_bytes, canonical_representation_anchor_bytes,
+    canonical_representation_plan_bytes,
 };
 pub use cost::{CostVectorError, ExactCost, ExactCostVector, ResidentCost, exact_cost_vector};
 pub use error::GraphError;
